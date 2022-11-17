5f15cbebe8f9737ffe9e5f5dcd5cc67adcdb1b77
terrobladeD <terrobladediao@gmail.com>
2022-11-18 00:03:35 +1100

    demo01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4f870358b9a1d192e492353b7a28cd02&page=1";
+// const IMAGES_API = "https://image.tmdb.org/t/p/w1280/";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=4f870358b9a1d192e492353b7a28cd02&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [canback, setCanback] = useState(false);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API).then(res => res.json()).then(data => {
+      setMovies(data.results);
+    });
+    setCanback(false);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+    setCanback(true);
+
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const hangleOnClick = ()=>{
+    getMovies(FEATURED_API);
+  }
+
+  const isDispalyBack = ()=>{
+    if (!canback){
+      return "canback"
+    }
+    else{
+      return ""
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className={['back',isDispalyBack()].join(' ')} onClick={hangleOnClick} >
+        <button>back</button></div>
+        <form onSubmit={handleOnSubmit}>
+          <input className='search' type="text" placeholder="Search..."
+            value={searchTerm} onChange={handleOnChange}
+          />
+        </form>
+
+
       </header>
-    </div>
-  );
+      <div className='movie-container'>
+
+        {movies.length > 0 && movies.map((movie) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>);
 }
 
 export default App;
---
+++ src/readme.md
@@ -0,0 +1,8 @@
+电影列表
+https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4f870358b9a1d192e492353b7a28cd02&page=1
+
+电影照片
+https://image.tmdb.org/t/p/w1280/b6IRp6Pl2Fsq37r9jFhGoLtaqHm.jpg
+
+搜索
+https://api.themoviedb.org/3/search/company?api_key=4f870358b9a1d192e492353b7a28cd02&query=heart&page=1
